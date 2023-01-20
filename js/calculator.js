bf81ef824cfd55b9e3b25808bcef9143c1428462
ArtemVolonin <volonin@ukr.net>
2023-01-20 22:07:27 +0200

    Доработка функционала кнопок

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,27 @@
 let DivButtons = document.querySelector(".buttons");
+//массив кнопок
 let arBtn = document.querySelectorAll(".btn");
+//экраны
 let main_string = document.querySelector(".main_string");
 let main_primer = document.querySelector(".main_primer");
+let main_history = document.querySelector(".history_res");
 let znak = '';
+let a = '';
+let b = '';
+let finish = false;
 
+const Digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+const sign = ['*', '/', '-', '+', '%', '+/-', '='];
+
+function ClearAll() {
+    a = '';
+    b = '';
+    finish = false;
+    main_string.textContent = '0';
+    main_primer.textContent = '';
+    //main_history.textContent = main_primer.textContent;
+}
+//навешиваем обработку событий на кнопки
 arBtn.forEach(btn => {
     //  console.log(btn);
     btn.addEventListener("mousedown", function () {
@@ -13,30 +31,97 @@ arBtn.forEach(btn => {
     btn.addEventListener("mouseup", function () {
         btn.classList.remove("keydown");
     })
-    btn.addEventListener("mouseleave", function () {       
+    btn.addEventListener("mouseleave", function () {
         btn.classList.remove("keydown");
     })
-   
-        btn.addEventListener("click", function() {  
-            if (main_string.textContent == "0")  main_string.textContent = '';        
-        main_string.textContent +=  btn.textContent;
-         })
+
+    btn.addEventListener("click", function () {
+        if (main_string.textContent == "0" && btn.textContent !== '.') main_string.textContent = '';
+        
+
+        //если нажата цифра - не знак то
+        if (Digits.includes(btn.textContent)) {
+            if (finish) {ClearAll() ; if ( btn.textContent == '.') main_string.textContent = '0';}
+            main_string.textContent += btn.textContent;
+            if (znak === '' && !finish) { a += btn.textContent;  console.log("a += btn.textContent")}  // запоминаем а
+            else if (znak !== '') { b += btn.textContent };//запоминаем b
+           // console.log(a, b, finish);
+        }
+       // после вычисления и нажатого равно - если нажата кнопка то если знак применяем к результату если число - начинаем сначала
+        if (finish) {
+            if (Digits.includes(btn.textContent)) {  
+                        ClearAll() ; 
+                        if ( btn.textContent == '.') main_string.textContent = '0' ; 
+                    }
+                   //если нажат знак тогда в а записывем первое число 
+            if (sign.includes(btn.textContent)) {
+               a = main_string.textContent;
+               b = '';
+              // znak = btn.textContent; 
+               finish = false;
+               console.log(a, b, znak, "finish");
+            }
+        }
+
+
+    })
 });
-document.querySelector(".buttons").addEventListener("click", function(e){
-    const avent = e.target.dataset.action;
-    if (avent) {ActionGo[avent](); }
+//вытягиваем знак
+document.querySelector(".buttons").addEventListener("click", function (e) {
+    const avent = e.target.dataset.action; //аттрибут data-action
+    if (avent) {
+
+        ActionGo[avent]();
+    }
+    // else {main_string.textContent +=  btn.textContent;}
 })
 
 let ActionGo = {
-    reset(){ main_string.textContent = "0"; znak='';  main_primer.textContent =''},
-    delenie(){ 
-        main_primer.textContent = main_string.textContent;
-        main_string.textContent = '0';
-        znak = "/";
+    reset() { ClearAll() },
+    delenie() {
+        znak = "/";  
+        main_string.textContent += znak;
     },
-    ravno(){
-        main_primer.textContent += main_string.textContent;
-         main_string.innerHTML = main_primer.textContent ;}   
+    umn() {
+        znak = "*";  
+        main_string.textContent += znak;
+    },
+    minus() {
+        znak = "-";  
+        main_string.textContent += znak;
+    },
+    plus() {
+        znak = "+";  
+        main_string.textContent += znak;
+    },
+    prc() {
+        znak = "%";  
+        main_string.textContent += znak;
+    },
+    ravno() {
+        main_primer.textContent = main_string.textContent;
+        RESULT()
+    },
+
+
+}
+
+function RESULT() {
+    if (b==='') b = a;
+    
+    switch (znak) {
+        case "/": { main_string.textContent =  Math.round((+a) / (+b)*100)/100;  break;}
+        case "+": { main_string.textContent = (+a) + (+b);  console.log(a, b, znak); break;}
+        case "-": { main_string.textContent = (+a) - (+b);   console.log(a, b, znak); break;}
+        case "%": { main_string.textContent = (Math.round(((+a) * 100)/(+b)*100)/100);  console.log(a, b, znak); break;}
+        case "*": { main_string.textContent = Math.round((+a) * (+b)*100)/100;  console.log(a, b, znak); break;}
+      //  case "/": { main_string.textContent = (+a) / (+b); console.log((+a) / (+b)); console.log(a, b, znak); break;}
+
+    }
+   // znak = '';
+    finish = true;
+    main_history.textContent = main_primer.textContent+'='+main_string.textContent;
+    console.log(a, b, znak, "ravno");
 }
 
 /* console.log(arButtons); */
